405c6ddd5a26da72f9769cde4c3d2aae7fcddb79
peter.parys <parys@uniquemail.net>
2017-04-04 15:53:46 +0200

    port adjusted

--- netko-exercise2/src/main/java/tcpeditor/Starter.java
+++ netko-exercise2/src/main/java/tcpeditor/Starter.java
@@ -7,7 +7,7 @@ public class Starter {
 	public static void main(String[] args) {
 		Model model = new Model();
 		model.setUrl("localhost");
-		model.setPort(80);
+		model.setPort(8080);
 		model.setModus(Modus.CLIENT);
 		View view = new View(model);
 		Controller controller = new Controller(model, view);
