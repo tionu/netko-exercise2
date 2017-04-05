572f957216e709c7ffcba7c0008c68bd2b46cf44
peter.parys <parys@uniquemail.net>
2017-04-05 18:35:44 +0200

    starter fixed

--- netko-exercise2/src/main/java/tcpeditor/Starter.java
+++ netko-exercise2/src/main/java/tcpeditor/Starter.java
@@ -4,15 +4,16 @@ import tcpeditor.Model.Modus;
 
 public class Starter {
 
-	public static void main(String[] args) {
-		Model model = new Model();
-		model.setUrl("localhost");
-		model.setPort(8080);
-		model.setModus(Modus.CLIENT);
-		View view = new View(model);
-		Controller controller = new Controller(model, view);
-		view.addActionListener(controller);
-		view.show();
-	}
+    public static void main(String[] args) {
+        Model model = new Model();
+        model.setUrl("localhost");
+        model.setPort(80);
+        model.setModus(Modus.CLIENT);
+        View view = new View(model);
+        Controller controller = new Controller(model, view);
+        model.addListener(view);
+        view.addActionListener(controller);
+        view.show();
+    }
 
 }
