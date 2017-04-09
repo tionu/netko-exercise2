a72ecfa53738fc84083dbe1053f43a7eeac25980
raikm <RaikMueller@raiks-mac-pro.fritz.box>
2017-04-09 10:27:56 +0200

    implements startServer and sendDate

--- netko-exercise2/src/main/java/tcpeditor/Model.java
+++ netko-exercise2/src/main/java/tcpeditor/Model.java
@@ -1,6 +1,11 @@
 package tcpeditor;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintStream;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -9,30 +14,32 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Model {
-	
+
 	public enum Modus {
 		CLIENT, SERVER
 	}
-	
+
 	public interface ModelListener {
-		
+
 		public void modelChanged();
-		
+
 	}
-	
+
 	private List<ModelListener> listeners;
 	private Modus modus;
 	private String url;
 	private int port;
 	private String textToSend;
 	private String receivedText;
-	private Socket socket; // Dieses Attribut benötigen Sie, wenn Sie den Modus SERVER programmieren, um einen Socket,
-	// der in der Methode startServer() erstellt wurde auch in der Methode sendData() verwenden zu können
-	
+	private Socket socket; // Dieses Attribut benötigen Sie, wenn Sie den Modus
+							// SERVER programmieren, um einen Socket,
+	// der in der Methode startServer() erstellt wurde auch in der Methode
+	// sendData() verwenden zu können
+
 	public Model() {
 		this.listeners = new LinkedList<>();
 	}
-	
+
 	public Modus getModus() {
 		return modus;
 	}
@@ -78,15 +85,38 @@ public class Model {
 	}
 
 	private void modelChanged() {
-		for(ModelListener listener : listeners)
+		for (ModelListener listener : listeners)
 			listener.modelChanged();
 	}
-	
-	public void sendData(){
-		// TODO
+
+	public void sendData() {
+		try {
+			OutputStream output = socket.getOutputStream();
+			PrintStream printstream = new PrintStream(output, true);
+			printstream.print(textToSend);
+			printstream.flush();
+		} catch (IOException e) {
+			System.err.println("IO Exception");
+		} catch (NullPointerException e){
+			System.err.println("keine Nachricht");
+		}
 	}
-	
+
 	public void startServer() {
-		// TODO
+		try {
+			ServerSocket serverSocket = new ServerSocket(port);
+			Socket socket = serverSocket.accept();
+			InputStream inputstream = socket.getInputStream();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(inputstream));
+			receivedText = "";
+			while (reader.ready())
+				receivedText += reader.readLine() + "\n";
+
+			serverSocket.close();
+			reader.close();
+			inputstream.close();
+		} catch (Exception e) {
+
+		}
 	}
 }
